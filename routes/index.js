847cdccbeaccce2cd5e853f0a3cda5fd98439ab3
Ryan <ryandoyle@me.com>
2018-11-02 19:25:16 -0700

    Update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,4 @@ router.get('/', (req, res) => {
   res.send('Hey! It works!');
 });
 
-<<<<<<< HEAD
-=======
-router.get('/reverse', (req, res) => {
-  res.send('Hey! Reverse works!');
-})
->>>>>>> d33fe28e240a8dcb326e9b2de27e124495aced84
-
 module.exports = router;
