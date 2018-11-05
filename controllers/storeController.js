bb9260dd2a581a6485a3a7c7236e72de7db58f84
Ryan <ryandoyle@me.com>
2018-11-04 16:52:12 -0800

    work on store cards

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,7 +10,18 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save(); //save the store to db and returns the store, or error
-  res.redirect('/');
+  const store = await (new Store(req.body)).save(); //save the store to db and returns the store, or error
+  req.flash('success', `Successfully created ${store.name}. Would you like to leave a review?`)
+  res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: "Stores", stores: stores })
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id })
+  res.render('editStore', { title: `Edit ${store.name}`, store}) //store is really like {store: store} but in es6 you can just put name
 }
--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,11 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // res.json();
 // res.render();
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 
 module.exports = router;
