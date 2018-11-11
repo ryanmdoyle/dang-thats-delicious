c9eb228410d43b9744b790191be66e11483a7405
Ryan <ryandoyle@me.com>
2018-11-11 12:41:08 -0800

    Added photos to update store route

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,10 @@ router.post('/add',
   catchErrors(storeController.createStore)
 );
 
-router.post('/add/:id', catchErrors(storeController.updateStore));
+router.post('/add/:id', 
+  storeController.upload, 
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 
